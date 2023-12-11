5bc83a6bdc9411171d607030487b1a707c0430b9
mustafadurmaz <mdmz_001@hotmail.com>
2023-12-11 23:28:44 +0300

    getAllUniversities service created

--- Client/src/app/components/UniversityDataTable.tsx
+++ Client/src/app/components/UniversityDataTable.tsx
@@ -1,10 +1,12 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { Controller, useForm } from "react-hook-form";
+
+import YKSService from "../../services/YKS/YKS";
 
 interface ITYTInputs {
   turkceDogru?: number;
@@ -27,11 +29,9 @@ interface Params {
   tytScores?: any;
 }
 
-const UniversityDataTable = ({
-  tytScores,
-}: Params) => {
-
-  const [customers, setCustomers] = useState([]);
+const UniversityDataTable = ({ tytScores }: Params) => {
+  const [universities, setUniversities] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const schema = yup.object({});
 
@@ -52,21 +52,66 @@ const UniversityDataTable = ({
     },
   });
 
+  const getUniversities = (isClear = false) => {
+    setLoading(true);
+    YKSService.getAllUniversities()
+      .then((response) => {
+        console.log("component response data",response.data);
+        setUniversities(response.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        // toast?.current?.show({
+        //   severity: "error",
+        //   summary: t("Error"),
+        //   detail: err.response ? err.response.data.message : err.message,
+        //   life: 3000,
+        // });
+        setLoading(false);
+      });
+  };
+  
+  useEffect(() => {
+    getUniversities();
+  },[]);
+
+
   const onSubmit = (data: any) => {
     console.log("data", data);
   };
 
-
   return (
     <>
       <div className="card">
-            <DataTable value={customers} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="name" header="Üniversite" style={{ width: '25%' }}></Column>
-                <Column field="country.name" header="Bölüm" style={{ width: '25%' }}></Column>
-                <Column field="company" header="Puan" style={{ width: '25%' }}></Column>
-                <Column field="representative.name" header="İl" style={{ width: '25%' }}></Column>
-            </DataTable>
-        </div>
+        <DataTable
+          value={universities}
+          paginator
+          rows={5}
+          rowsPerPageOptions={[5, 10, 25, 50]}
+          tableStyle={{ minWidth: "50rem" }}
+        >
+          <Column
+            field="UniversiteAdi"
+            header="Üniversite"
+            style={{ width: "25%" }}
+          ></Column>
+          <Column
+            field="ProgramAdi"
+            header="Bölüm"
+            style={{ width: "25%" }}
+          ></Column>
+          <Column
+            field="TabanPuan"
+            header="Puan"
+            style={{ width: "25%" }}
+          ></Column>
+          <Column
+            field="Sehir"
+            header="İl"
+            style={{ width: "25%" }}
+          ></Column>
+        </DataTable>
+      </div>
     </>
   );
 };
--- Client/src/services/YKS/YKS.ts
+++ Client/src/services/YKS/YKS.ts
@@ -1,1 +1,14 @@
 import axios from "axios";
+
+const url = "http://localhost:3000/yks-lisans";
+
+const getAllUniversities = async () => {
+    return axios.get(url);
+  };
+
+
+  const service = {
+    getAllUniversities
+  };
+  
+  export default service;
