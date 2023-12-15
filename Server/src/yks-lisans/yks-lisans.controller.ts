c0f2ce90f610b3c78f47f799430a7abeeb7d05bd
mustafadurmaz <mdmz_001@hotmail.com>
2023-12-15 23:36:15 +0300

    added to filter get universities service

--- Server/src/yks-lisans/yks-lisans.controller.ts
+++ Server/src/yks-lisans/yks-lisans.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Get, HttpStatus } from '@nestjs/common';
-import { YksLisansService } from './yks-lisans.service';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  ParseIntPipe,
+  Query,
+} from "@nestjs/common";
+import { YksLisansService } from "./yks-lisans.service";
 
-@Controller('yks-lisans')
+@Controller("yks-lisans")
 export class YksLisansController {
-    constructor(private yksLisansService: YksLisansService) {}
-    @Get()
-    async showAllUniversities(){
-        return {
-            statusCode:HttpStatus.OK,
-            data: await this.yksLisansService.showAllUniversities(),
-        }
-    }
+  constructor(private yksLisansService: YksLisansService) {}
+
+  // @Get()
+  // async showAllUniversities(){
+  //     return {
+  //         statusCode:HttpStatus.OK,
+  //         data: await this.yksLisansService.showAllUniversities(),
+  //     }
+  // }
+
+  @Get("universities")
+  async filterUsers(
+    @Query("name") name: string,
+    @Query("email") email: string,
+    @Query("page", ParseIntPipe) page = 1,
+    @Query("limit", ParseIntPipe) limit = 10
+  ) {
+    const filters = { name, email };
+    const result = await this.yksLisansService.filterUsers(
+      filters,
+      page,
+      limit
+    );
+    return result;
+  }
 }
--- Server/src/yks-lisans/yks-lisans.service.ts
+++ Server/src/yks-lisans/yks-lisans.service.ts
@@ -1,17 +1,45 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { YksLisansEntity } from './entity/yks-lisans.entity';
-import { Repository } from 'typeorm';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { YksLisansEntity } from "./entity/yks-lisans.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class YksLisansService {
-    constructor(
-        @InjectRepository(YksLisansEntity)
-        private yksLisansRepository: Repository<YksLisansEntity>,
-    ){}
-    async showAllUniversities(){
-        return await this.yksLisansRepository.find({
-            take: 100, 
-        });
+  constructor(
+    @InjectRepository(YksLisansEntity)
+    private yksLisansRepository: Repository<YksLisansEntity>
+  ) {}
+
+  // async showAllUniversities(){
+  //     return await this.yksLisansRepository.find({
+  //         take: 100,
+  //     });
+  // }
+
+  async filterUsers(
+    filters: Record<string, any>,
+    page = 1,
+    limit = 10
+  ): Promise<{ universities: YksLisansEntity[]; total: number }> {
+    const query = this.yksLisansRepository.createQueryBuilder("yks");
+
+    if (filters.Sehir) {
+      query.andWhere("user.Sehir LIKE :Sehir", { Sehir: `%${filters.Sehir}%` });
     }
+
+    if (filters.email) {
+      query.andWhere("user.email LIKE :email", { email: `%${filters.email}%` });
+    }
+
+    // Diğer filtreler için de benzer şekilde devam edilebilir.
+
+    const total = await query.getCount(); // Toplam kayıt sayısını alır
+
+    const universities = await query
+      .take(limit) // Her sayfada gösterilecek kullanıcı sayısı
+      .skip((page - 1) * limit) // Atlanacak kayıt sayısı
+      .getMany();
+
+    return { universities, total };
+  }
 }
