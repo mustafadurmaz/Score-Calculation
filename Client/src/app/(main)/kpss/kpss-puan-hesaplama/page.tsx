dbf2df6d67b20ab13ee3fa6fd8389318efe72733
mustafadurmaz <mdmz_001@hotmail.com>
2024-02-01 23:50:50 +0300

    kpss file created

---
+++ Client/src/app/(main)/kpss/kpss-puan-hesaplama/page.tsx
@@ -0,0 +1,93 @@
+"use client";
+import React, { useState } from "react";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
+import { Button } from "primereact/button";
+import { InputTextarea } from "primereact/inputtextarea";
+import { Dropdown } from "primereact/dropdown";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { Controller, useForm } from "react-hook-form";
+import TYTPuanHesaplama from "../../../components/TYTPuanHesaplama";
+import AYTPuanHesaplama from "../../../components/AYTPuanHesaplama";
+import PuanSonucları from "../../../components/PuanSonucları";
+import UniversityDataTable from "../../../components/UniversityDataTable";
+
+const YKSPage = () => {
+  const [toggleReset, setToggleReset] = useState<boolean>(true);
+  const [tytScores, setTytScores] = useState({
+    turkceNet: 0,
+    sBilimlerNet: 0,
+    tMatematikNet: 0,
+    fBilimleriNet: 0,
+    diplomaNotu: 0,
+    isEnroll:false
+  });
+
+  const [aytScores, setAytScores] = useState({
+    edebiyatNet: 0,
+    tarih1Net: 0,
+    cografya1Net: 0,
+    tarih2Net: 0,
+    cografya2Net: 0,
+    felsefeNet: 0,
+    dinNet: 0,
+    matematikNet: 0,
+    fizikNet: 0,
+    kimyaNet: 0,
+    biyolojiNet: 0,
+  });
+
+  const schema = yup.object({});
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {},
+  });
+
+
+
+  const onSubmit = (data: any) => {
+
+  };
+
+  return (
+    <>
+      <div className="grid">
+        <div className="col-12 md:col-4">
+          <TYTPuanHesaplama
+            setTytScores={setTytScores}
+            toggleReset={toggleReset}
+          />
+        </div>
+        <div className="col-12 md:col-4">
+          <AYTPuanHesaplama setAytScores={setAytScores} toggleReset={toggleReset}/>
+        </div>
+        <div className="col-12 md:col-4">
+          <PuanSonucları
+            tytScores={tytScores}
+            setTytScores={setTytScores}
+            aytScores={aytScores}
+            setAytScores={setAytScores}
+            toggleReset={toggleReset}
+            setToggleReset={setToggleReset}
+          />
+        </div>
+
+        {/* <div className="col-12">
+          <UniversityDataTable />
+        </div> */}
+      </div>
+    </>
+  );
+};
+
+export default YKSPage;
